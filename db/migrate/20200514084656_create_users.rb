e884126716fa49b4b316205cae2ba04d25fd7e99
AdrienCastelbou <acastelbou1@gmail.com>
2020-05-14 11:04:35 +0200

    devise ok

---
+++ db/migrate/20200514084656_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.text :description
+      t.belongs_to :city, index: true 
+      t.timestamps
+    end
+  end
+end
